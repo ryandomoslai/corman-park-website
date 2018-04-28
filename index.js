1d0e08d9ad0d521d8e372b547f78bcb8f2fe2e24
ryandomo <ryanisawesome102@gmail.com>
2018-04-27 22:08:32 -0600

    Created accordian style team bio page, with modal detailed bios.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function moveSection(elementName) {
+    console.log(elementName);
     var element = document.getElementById(elementName);
     $('html, body').animate({
         scrollTop: $(element).offset().top
-    }, 800);
+    }, 500);
 }
