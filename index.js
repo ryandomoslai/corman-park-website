695e350d4e4b51d462e8145bc88588fcb9860342
ryandomoslai <ryanwork277@gmail.com>
2018-09-19 22:49:31 -0600

    Updated descriptions, bios

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var people = [
+    // Vets
     {
         "name": "Harvey",
-        "image": "./img/Harvey.jpg",
-        "position": "Lead Vet & Owner",
+        "image": "./img/Staff/Harvey.jpg",
+        "position": "Veterinarian & Owner",
         "bio": "Harvey is from Whitewood, SK but currently lives in Saskatoon, SK (?? Not a farm near Saskatoon). He attended the Western College of Veterinary Medicine, Saskatoon, SK and graduated in 1996. He is a Husband (Lisa) and father ( Kyla & Ryan).<br>" 
         + "He was raised on a farm and was around both large and small animals all his life. Although he feels becoming a Veterinarian was a natural progression for him, he joined the Navy and spent 10 yrs as an Officer before deciding on veterinary schooling.Once he graduated, he worked in Watrus, SK, Prince Albert, SK and finally Saskatoon, SK before practising at Corman Park Veterinary Services. In 2003, Harvey purchased the clinic from CPVS founder Wally Kononof, becoming both Veterinarian and owner. His favorite part of the job is the variety as every day is different.<br>" 
         + "Other hobbies –<br>"
@@ -10,78 +11,77 @@ var people = [
     },
     {
         "name": "Inoka",
-        "image": "./img/Inoka.jpg",
+        "image": "./img/Staff/Inoka.jpg",
         "position": "Veterinarian",
-        "bio": "Owner of Corman Park"
+        "bio": "Graduated in 2002 from University of Peradeniya in Sri Lanka. Moved to Saskatoon in 2009 and completed an MSc in Ruminant Nutrition in 2012. Attended the Veterinary Skill Training Enhancement Program at  Ontario Veterinary College in 2016. Currently lives in Saskatoon with her husband, two sons, two cats and one dog."
     },
     {
-        "name": "Bunnie",
-        "image": "./img/Bunnie.jpg",
-        "position": "Owner / Veterinarian",
-        "bio": "Owner of Corman Park"
+        "name": "Blair",
+        "image": "./img/Staff/Generic.jpg",
+        "position": "Veterinarian",
+        "bio": "Grew up in Outlook, Saskatchewan and graduated WCVM with Dr. Domoslai in 1996. He is an independent veterinary contractor who specializes in bovine reproduction including semen and embryo collections, as well as helping out with the day to day mixed animal practice at CPVS."
     },
     {
-        "name": "Christina",
-        "image": "./img/Christina.jpg",
-        "position": "Owner / Veterinarian",
-        "bio": "Owner of Corman Park"
+        "name": "Amanda",
+        "image": "./img/Staff/Generic.jpg",
+        "position": "Veterinarian",
+        "bio": "Graduated WCVM and worked in Swift Current until she moved north of Saskatoon where she specializes in dairy medicine. She is presently raising two young children on her farm with her husband Greg."
     },
+    {
+        "name": "Dawalla",
+        "image": "./img/Staff/Dawalla.jpg",
+        "position": "Veterinarian",
+        "bio": "Born in Sri Lanka. Moved to Canada at the age of 15. He has been in Saskatoon ever since. Attended Centennial Collegiate in Saskatoon for high school. After that, completed two years of an Animal Bioscience degree prior to entering WCVM. He very much enjoys the variety of cases that mixed animal practice has to offer. Hobbies include travelling, scuba diving and cricket."
+    },
+    {
+        "name": "Allison Sprout",
+        "image": "./img/Staff/Generic.jpg",
+        "position": "Veterinarian",
+        "bio": "Graduated WCVM and recently completed a one year internship in equine surgery and medicine. She returned to Saskatoon this spring and is a key member of the CPVS team of Veterinarians, working with all animals large and small."
+    },
+    {
+        "name": "Lisa Weyman",
+        "image": "./img/Staff/Generic.jpg",
+        "position": "Veterinarian",
+        "bio": "Graduated WCVM with Dr. Domoslai in 1996. She operated a mobile equine practice for many years in the Saskatoon area before joining CPVS as an equine veterinarian. She recently took a position at the Canadian Food Inspection Agency and now works part time with CPVS."
+    },
+    // Techs
+    {
+        "name": "Jodie",
+        "image": "./img/Staff/Jobie.jpg",
+        "position": "Technician",
+        "bio": ""
+    },
+    {
+        "name": "Aspen",
+        "image": "./img/Staff/Aspen.jpg",
+        "position": "Technician",
+        "bio": "Graduate of the Saskatchewan Polytechnic Veterinary Technology program. Provides assistance in all scopes of practice with strengths in bovine and equine."
+    },
+    {
+        "name": "Keana",
+        "image": "./img/Staff/Generic.jpg",
+        "position": "Technician",
+        "bio": "Graduate of the Saskatchewan Polytechnic Veterinary Technology program. Worked in a number of small animal practices in Saskatoon before joining CPVS."
+    },
+    // Management
     {
         "name": "Dorrie",
-        "image": "./img/Dorrie.jpg",
-        "position": "Other vet",
-        "bio": "Other vet of Corman Park"
+        "image": "./img/Staff/Dorrie.jpg",
+        "position": "Management",
+        "bio": "Was born and raised on a mixed farm by Strasbourg. After graduating high school I obtained diplomas in professional cooking, animal science, vet tech and interior design. In 2007, started working for CPVS and also purchased a house in the town of Asquith. In 2016, purchased an acreage alongside her husband Ryan just outside of Asquith, where they currently reside."
     },
     {
-        "name": "Jill",
-        "image": "./img/Jill.jpg",
-        "position": "Other vet",
-        "bio": "Other vet of Corman Park"
+        "name": "Megan H",
+        "image": "./img/Staff/Generic.jpg",
+        "position": "Management",
+        "bio": "Provides accounting and book keeping services to CPVS. Resides north of Saskatoon where she and her husband Kyle raise their family and thoroughbred horses. "
     },
-    {
-        "name": "Jobie",
-        "image": "./img/Jobie.jpg",
-        "position": "Vet Tech",
-        "bio": "Jobie is from Warman, SK who currently lives in St. Isidore de Bellevue, SK running a cattle ranch with her boyfriend (Sam). She attended Lakeland Collage in Vermillion, AB and graduated from the Animal Health Technician program in 2011.<br>"
-        + "She recalls always wanting to be a Veterinarian but later learned of the Animal Health Technology program and that with it she would be in a positon to have far more interaction and hands-on work with the animals versus as a Veterinarian. It was because she loves to see the human/animal bond, loves to interact with the owners of pets and her passion for animals that made her realize she would rather be an Animal Health Technician.<br>"
-        + "Jobie became a part of the Corman Park Veterinary Services family in Sept of 2016.  Her favorite parts of her job are the challenges of collecting blood and placing catheters as well as hearing client stories about their pets.<br>"
-        + "She keeps herself busy with many things other than working at the clinic. With a new Cattle Ranch she is learning to manage/run the ranch including learning to rope and treat cattle. She enjoys horse-back riding, reading (if time allows), taking/editing photos and putting them in albums. As well, she has been learning about essential oils and their benefits for both humans and animals.  She shares that knowledge through Youngliving Essential Oils. Although, one of her biggest highlights is “Face-timing” with her niece who lives in Missouri.<br>"
-        + "There are many four-legged furbabies that make up Jobie and Sam’s family. Lucy a Red Heeler, Ace a Quarter Horse mare, Buck a Welsh Pony/Quarter Horse. Chuck & Spud - cattle working dogs, as well as 6 other horses. Then there is Polly a cat with a story around how she became a member of Jobie’s home.  Polly had been surrendered to the clinic and Jobie hated to see her sitting in a kennel for days while waiting for a new home. One night Jobie took her home for a break from the clinic kennel and Polly never left Jobie’s home again. Of course, she loves to spend time playing with every one of the pets."
-    },
-    {
-        "name": "Mel",
-        "image": "./img/Mel.jpg",
-        "position": "Other vet",
-        "bio": "Other vet of Corman Park"
-    },
-    {
-        "name": "Pat",
-        "image": "./img/Pat.jpg",
-        "position": "Other vet",
-        "bio": "Other vet of Corman Park"
-    },
-    {
-        "name": "Roy",
-        "image": "./img/Roy.jpg",
-        "position": "Other vet",
-        "bio": "Other vet of Corman Park"
-    },
-    {
-        "name": "Steph",
-        "image": "./img/Steph.jpg",
-        "position": "Other vet",
-        "bio": "Other vet of Corman Park"
-    },
-    {
-        "name": "Tisha",
-        "image": "./img/Tisha.jpg",
-        "position": "Other vet",
-        "bio": "Other vet of Corman Park"
-    },
+    // Support
+
 ]
 
 function moveSection(elementName) {
-    console.log(elementName);
     let element = document.getElementById(elementName);
     $('html, body').animate({
         scrollTop: $(element).offset().top
@@ -107,7 +107,6 @@ function setHoverText(personName) {
 }
 
 function setTeamGallery() {
-    console.log("We made it here");
     var teamContainer = document.getElementById("team-photos");
     var row = document.createElement("div");
     row.classList.add("row");
